c504ef1dda8307bb3ccefba138e58af6ac9fca50
viswav3428-tech <viswa.v3428@gmail.com>
2026-09-19 14:10:48 +0530

    Change host to 0.0.0.0 for uvicorn server
    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
     port change to
    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

--- main.py
+++ main.py
@@ -447,6 +447,6 @@ def get_session_messages(session_id: int):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
